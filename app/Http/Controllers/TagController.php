793b2645c3193e7a900bd2088d0a787c417ad764
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-04-20 13:38:00 +0330

    finish tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,43 +12,43 @@ use App\Repository\valueRepo;
 class TagController extends Controller
 {
 
-    public function __construct(public tagRepo $tagRepo){}
+    public function __construct(public tagRepo $tagRepo)
+    {
+    }
 
     public function index()
     {
         return $this->tagRepo->index();
     }
 
-    public function store(StoreTagRequest $request , attributeRepo $attributeRepo , valueRepo $valueRepo)
+    public function store(StoreTagRequest $request, attributeRepo $attributeRepo, valueRepo $valueRepo)
     {
         $attributeRepo = $attributeRepo->getfindId(array_keys($request->all()));
-        $valueRepo = $valueRepo->getFindValue($request->all() , $attributeRepo->id);
-        $createTags = $this->tagRepo->create($attributeRepo->id , $valueRepo->id);
-
-
-        return response()->json(['ok'],200);
+        $valueRepo = $valueRepo->getFindValue($request->all(), $attributeRepo->id);
+        $this->tagRepo->create($attributeRepo->id, $valueRepo->id);
+        return response()->json(['ok'], 200);
     }
 
 
     public function show($tag)
     {
-        //
+        return $this->tagRepo->getFindId($tag);
+    }
+
+    public function update(UpdateTagRequest $request, $tag , attributeRepo $attributeRepo, valueRepo $valueRepo)
+    {
+        $attributeRepo = $attributeRepo->getfindId(array_keys($request->all()));
+        $valueRepo = $valueRepo->getFindValue($request->all(), $attributeRepo->id);
+
+        $this->tagRepo->update($tag , $attributeRepo->id, $valueRepo->id);
+        return response()->json( ['ok'] , 200);
     }
 
 
-    public function edit($tag)
+    public function destroy($tag)
     {
-        //
-    }
-
-    public function update(UpdateTagRequest $request, $tag)
-    {
-        //
-    }
-
-
-    public function destroy(Tag $tag)
-    {
-        //
+        $deleted  = $this->tagRepo->delete($tag);
+            if($deleted === 0 ) return response()->json(['not found id'],200);
+        return response()->json(['ok'],200);
     }
 }
--- app/Repository/tagRepo.php
+++ app/Repository/tagRepo.php
@@ -18,7 +18,25 @@ class tagRepo
         $jsonData = json_encode($data);
         return Tag::query()->create([
             'attr' => $jsonData ,
-
         ]);
+    }
+
+    public function getFindId($id)
+    {
+        return Tag::query()->where('id' , $id)->first();
+    }
+
+    public function update($id , $attr , $value)
+    {
+        $data = [$attr, $value];
+        $jsonData = json_encode($data);
+        return Tag::query()->where('id' , $id)->update([
+            'attr' => $jsonData ,
+        ]);
+    }
+
+    public function delete($id)
+    {
+        return Tag::query()->where('id' , $id)->delete();
     }
 }
